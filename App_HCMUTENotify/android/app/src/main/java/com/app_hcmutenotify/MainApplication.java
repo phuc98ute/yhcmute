421639ce69fee00fe4f3db23df6e888a668ef9e5
phuc98itute <16110423@student.hcmute.edu.vn>
2019-04-07 02:51:25 +0700

    add menu

--- App_HCMUTENotify/android/app/src/main/java/com/app_hcmutenotify/MainApplication.java
+++ App_HCMUTENotify/android/app/src/main/java/com/app_hcmutenotify/MainApplication.java
@@ -3,6 +3,8 @@ package com.app_hcmutenotify;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wix.interactable.Interactable;
+import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new Interactable(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
             new VectorIconsPackage(),
